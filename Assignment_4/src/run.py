b03d9425de8049f37dbb55c1c1ffce59cda7cbaa
Hellod035 <hellod035@163.com>
2024-07-26 14:49:50 +0800

    1

--- Assignment_4/src/run.py
+++ Assignment_4/src/run.py
@@ -142,12 +142,14 @@ elif args.function == 'finetune':
 
     ### YOUR CODE HERE ###
     finetune_text = open(args.finetune_corpus_path, encoding='utf-8').read()
-    finetune_dataset = dataset.CharCorruptionDataset(finetune_text, block_size)
+    finetune_corruption_dataset = dataset.CharCorruptionDataset(finetune_text, block_size)
+    finetune_name_dataset = dataset.NameDataset(finetune_corruption_dataset,
+            finetune_text)
     # initialize a trainer instance and kick off training
     tconf = trainer.TrainerConfig(max_epochs=2, batch_size=512, learning_rate=6e-4,
-                        lr_decay=True, warmup_tokens=512*20, final_tokens=2*len(finetune_dataset)*block_size,
+                        lr_decay=True, warmup_tokens=512*20, final_tokens=2*len(finetune_name_dataset)*block_size,
                         num_workers=4)
-    trainer = trainer.Trainer(model, finetune_dataset, None, tconf)
+    trainer = trainer.Trainer(model, finetune_name_dataset, None, tconf)
     trainer.train()
     ### END YOUR CODE ###
 elif args.function == 'evaluate':
